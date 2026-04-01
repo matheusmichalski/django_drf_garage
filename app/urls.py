81b5fd95799c9488abfc26b82a515cb9272315f9
matheusmichalski <matheus.michalskifc@gmail.com>
2026-04-01 15:54:54 -0300

    REFACTOR: standardize string quotes and improve user model field definitions

--- app/urls.py
+++ app/urls.py
@@ -23,32 +23,32 @@ from core.views import (
 
 router = DefaultRouter()
 
-router.register(r'usuarios', UserViewSet, basename='usuarios')
-router.register(r'accessories', AccessoryViewSet, basename='accessories')
-router.register(r'colors', ColorViewSet, basename='colors')
-router.register(r'models', VehicleModelViewSet, basename='models')
-router.register(r'vehicles', VehicleViewSet, basename='vehicles')
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"accessories", AccessoryViewSet, basename="accessories")
+router.register(r"colors", ColorViewSet, basename="colors")
+router.register(r"models", VehicleModelViewSet, basename="models")
+router.register(r"vehicles", VehicleViewSet, basename="vehicles")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # OpenAPI 3
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        'api/doc/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
+        "api/doc/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
     path(
-        'api/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
     # Autenticação JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     # Registro de usuários
-    path('api/registro/', UserRegistrationView.as_view(), name='user_registration'),
+    path("api/registro/", UserRegistrationView.as_view(), name="user_registration"),
     # API
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
