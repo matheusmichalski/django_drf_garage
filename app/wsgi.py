28cf8a07486f6b8f4f4dd55cf17ee2f1062120df
matheusmichalski <matheus.michalskifc@gmail.com>
2026-05-15 13:25:57 -0300

    FIX: correct punctuation in docstring and standardize string quotes in wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see:
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
